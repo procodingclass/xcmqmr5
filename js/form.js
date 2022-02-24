063c2b3985038da9c73f27396fbeaf3a96d21d67
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-02-24 22:41:00 +0530

    Update form.js

--- js/form.js
+++ js/form.js
@@ -6,10 +6,11 @@ class Form {
     this.greetings2 = createElement("h2");
   }
   display() {
-    this.input.position(displayWidth / 2 - 90, displayHeight / 2 - 150);
+    this.input.position(380, 180);
     this.input.size(180, 30);
-    this.buttonImg.position(displayWidth / 2 + 50, displayHeight / 2 - 100);
+    this.buttonImg.position(475 + 50, 250);
     this.buttonImg.size(150, 50);
+
 
     this.buttonImg.mousePressed(() => {
       console.log();
@@ -23,7 +24,7 @@ class Form {
       player.addPlayer();
       player.updateCount(playerCount);
       this.greetings.html("Hello " + player.name);
-      this.greetings.position(displayWidth / 2 - 200, displayHeight / 2 - 150);
+      this.greetings.position(300, 180);
     });
   }
 
@@ -33,10 +34,9 @@ class Form {
     this.greetings.hide();
   }
   end() {
-       this.greetings2.html(
+      this.greetings2.html(
       "Congratulations " + player.name + " Your Rank is " + player.rank
     );
-    this.greetings2.class("greeting");
-    this.greetings2.position(displayWidth / 2 - 100, displayHeight / 2 - 50);
+    this.greetings2.position(300, 185);
   }
 }
